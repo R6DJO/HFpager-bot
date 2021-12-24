1c6c072a9698a616661b0cec7ba274920003e4a3
Alexander Falin <alexander.falin@megafon.ru>
2021-12-24 22:49:58 +0300

    Update message some fix

--- bot4.py
+++ bot4.py
@@ -46,16 +46,16 @@ def hfpager_bot():
         last_dir = sorted(msg_dirs)[-1]
         nowt = time.time()
         if os.path.isdir(last_dir):
-            # try:
-            for filename in os.scandir(last_dir):
-                if os.stat(filename).st_ctime > nowt - 6:
-                    mesg = open(filename, 'r',
-                                encoding='cp1251')
-                    text = mesg.read()
-                    parse_file(filename.path.replace(pager_dir, ''), text)
-            # except Exception as ex:
-            #     now = date_time_now()
-            #     print(f'{now} HFpager send/receive message error: {ex}')
+            try:
+                for filename in os.scandir(last_dir):
+                    if os.stat(filename).st_ctime > nowt - 6:
+                        mesg = open(filename, 'r',
+                                    encoding='cp1251')
+                        text = mesg.read()
+                        parse_file(filename.path.replace(pager_dir, ''), text)
+            except Exception as ex:
+                now = date_time_now()
+                print(f'{now} HFpager send/receive message error: {ex}')
         time.sleep(5)
 
 
