c925317bf72281c922803048d1124cb4250eedaa
Alexander Falin <alexander.falin@megafon.ru>
2022-01-20 19:41:03 +0300

    Batt satus

--- bot4.py
+++ bot4.py
@@ -270,10 +270,10 @@ def send_bat_status(message):
     b_current = battery['current']
     b_temp = battery['temperature']
     bot.reply_to(message, f"""
-Уровень заряда батареи: {b_level}\n
-Статус батареи: {b_status}\n
-Температура: {b_temp}\n
-Ток потребления: {b_current}
+Уровень заряда батареи: {b_level}%
+Статус батареи: {b_status}
+Температура: {b_temp}°C
+Ток потребления: {b_current}mA
 """)
 
 
