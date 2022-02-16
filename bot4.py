5516533aaf30e65845622be637df1de43f591781
unknown <xfalins@gamil.com>
2022-02-16 22:57:35 +0300

    Some fix

--- bot4.py
+++ bot4.py
@@ -33,24 +33,24 @@ def date_time_now():
 
 def bot_polling():
     now = date_time_now()
-    logging.info(f'{now} Bot polling is running')
+    logging.info(f'Bot polling is running')
     while True:
         try:
             bot.polling(interval=5)
         except Exception as ex:
             now = date_time_now()
-            logging.error(f'{now} Bot polling error: {ex}')
+            logging.error(f'Bot polling error: {ex}')
             logging.debug(f'Error: {ex}', exc_info=True)
 
 
 def hfpager_bot():
     now = date_time_now()
-    logging.info(f'{now} HFpager run')
+    logging.info(f'HFpager run')
     subprocess.Popen(
         'am start --user 0 '
         '-n ru.radial.nogg.hfpager/ru.radial.full.hfpager.MainActivity ',
         stdout=subprocess.PIPE, shell=True)
-    logging.info(f'{now} HFpager message parsing is running')
+    logging.info(f'HFpager message parsing is running')
     power_stat_prev = 'UNKNOWN'
     while True:
         try:
@@ -70,12 +70,12 @@ def hfpager_bot():
                         parse_file(filename.path.replace(pager_dir, ''), text)
         except Exception as ex:
             now = date_time_now()
-            logging.error(f'{now} HFpager send/receive message error: {ex}')
+            logging.error(f'HFpager send/receive message error: {ex}')
             logging.debug(f'Error: {ex}', exc_info=True)
         power_stat = power_status()
         if power_stat != power_stat_prev:
             now = date_time_now()
-            logging.info(f'{now} Power status: {power_stat}')
+            logging.info(f'Power status: {power_stat}')
             bot.send_message(chat_id=chat_id,
                                   text=f'Power status: {power_stat}')
             power_stat_prev = power_stat
@@ -108,30 +108,30 @@ def parse_file(dir_filename, text):
     now = date_time_now()
     short_text = shorten(text, width=35, placeholder="...")
     if re.match(r'\d{6}-RO-0.+_' + str(my_id) + '.TXT', filename):
-        logging.info(f'{now} HFpager private message received: {text}')
+        logging.info(f'HFpager private message received: {text}')
         bot.send_message(chat_id=chat_id,
                          text=text)
         detect_request(text)
     elif re.match(r'\d{6}-RO-[2,3].+_' + str(my_id) + '.TXT', filename):
-        logging.info(f'{now} HFpager private message received and acknowledgment '
+        logging.info(f'HFpager private message received and acknowledgment '
               f'sent: {short_text}')
         bot.send_message(chat_id=chat_id, text=f'√ {text}')
         detect_request(text)
     elif re.match(r'\d{6}-R', filename):
-        logging.info(f'{now} HFpager message intercepted: {text}')
+        logging.info(f'HFpager message intercepted: {text}')
         bot.send_message(chat_id=chat_id, text=text,
                          disable_notification=True)
         detect_request(text)
     elif re.match(r'\d{6}-S[1-9]-\dP', filename):
-        logging.info(f'{now} HFpager message sent and acknowledgment '
+        logging.info(f'HFpager message sent and acknowledgment '
               f'received: {short_text}')
         send_edit_msg(key, f'√ {text}')
     elif re.match(r'\d{6}-S[1-9]-\dN', filename):
-        logging.info(f'{now} HFpager message sent and not '
+        logging.info(f'HFpager message sent and not '
               f'acknowledgment received: {short_text}')
         send_edit_msg(key, f'X {text}')
     elif re.match(r'\d{6}-S[1-9]-\d0', filename):
-        logging.info(f'{now} HFpager message sent: {short_text}')
+        logging.info(f'HFpager message sent: {short_text}')
         send_edit_msg(key, f'{text}')
 
 
@@ -153,7 +153,7 @@ def detect_request(text):
         mlon = match[2]
         message = ('https://www.openstreetmap.org/?'
                    f'mlat={mlat}&mlon={mlon}&zoom=12')
-        logging.info(f'{now} HFpager -> MapLink: {message}')
+        logging.info(f'HFpager -> MapLink: {message}')
         bot.send_message(chat_id=chat_id, text=message)
     # парсим =w{lat},{lon}: weather -> hf
     match = re.search(r'^=w(-{0,1}\d{1,2}\.\d{1,6}),(-{0,1}\d{1,3}\.\d{1,6})',
@@ -161,7 +161,7 @@ def detect_request(text):
     if match and mesg_to == str(my_id):
         mlat = match[1]
         mlon = match[2]
-        logging.info(f'{now} HFpager -> Weather: {mlat} {mlon}')
+        logging.info(f'HFpager -> Weather: {mlat} {mlon}')
         bot.send_message(chat_id=chat_id,
                          text=(f'{my_id}>{mesg_from} '
                                f'weather in: {mlat} {mlon}'))
@@ -181,7 +181,7 @@ def get_weather(lat, lon):
     if 'cod' in data:
         now = date_time_now()
         error = data['message']
-        logging.error(f'{now} HFpager get weather error: {error}')
+        logging.error(f'HFpager get weather error: {error}')
         return 'Error in weather'
     else:
         for day in data['daily'][:3]:
@@ -246,7 +246,7 @@ def parse_for_pager(message, abonent_id):
 def pager_transmit(message, abonent_id, repeat):
     now = date_time_now()
     short_text = shorten(message, width=35, placeholder="...")
-    logging.info(f'{now} HFpager send to ID:{abonent_id} repeat:{repeat} '
+    logging.info(f'HFpager send to ID:{abonent_id} repeat:{repeat} '
           f'message: {short_text}')
     subprocess.Popen(
         'am start --user 0 '
@@ -270,7 +270,7 @@ def power_status():
         
     except Exception as ex:
         now = date_time_now()
-        logging.error(f'{now} HFpager battery-status error: {ex}')
+        logging.error(f'HFpager battery-status error: {ex}')
         b_status = 'UNKNOWN'
     return b_status
 
@@ -310,7 +310,7 @@ def send_bat_status(message):
         
     except Exception as ex:
         now = date_time_now()
-        logging.error(f'{now} HFpager battery-status error: {ex}')
+        logging.error(f'HFpager battery-status error: {ex}')
         bot.reply_to(message, 'Статус питания недоступен :-(')
 
 @bot.message_handler(func=lambda message: True)
@@ -322,7 +322,7 @@ def echo_message(message):
         match = re.match(reg, message.text)
         if match:
             short_text = shorten(message.text, width=35, placeholder="...")
-            logging.info(f'{now} Bot receive message: {short_text}')
+            logging.info(f'Bot receive message: {short_text}')
             if match.group(2):
                 text_parse = match.group(2) + match.group(3)
             else:
