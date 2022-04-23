936f9811203e92c59d3b54a2906b8a107062120a
Alexander Falin <alexander.falin@megafon.ru>
2022-04-23 22:50:29 +0300

    Add beacone

--- bot4.py
+++ bot4.py
@@ -56,28 +56,37 @@ def hfpager_bot():
             pager_dir = ('/data/data/com.termux/files/home/storage/shared/'
                          'Documents/HFpager/')
             # pager_dir = '/storage/emulated/0/Documents/HFpager/'
-            msg_dirs = [f.path for f in os.scandir(pager_dir)
-                        if f.is_dir() and re.match(r'.*\.MSG', f.name)]
-            last_dir = sorted(msg_dirs)[-1]
+            # msg_dirs = [f.path for f in os.scandir(pager_dir)
+            #             if f.is_dir() and re.match(r'.*\.MSG', f.name)]
+            # last_dir = sorted(msg_dirs)[-1]
+            # nowt = time.time()
+            # if os.path.isdir(last_dir):
+            #     for filename in os.scandir(last_dir):
+            #         if os.stat(filename).st_ctime > nowt - 6:
+            #             mesg = open(filename, 'r',
+            #                         encoding='cp1251')
+            #             text = mesg.read()
+            #             parse_file(filename.path.replace(pager_dir, ''), text)
+            # msg_dirs = [f.path for f in os.scandir(pager_dir)
+            #             if f.is_dir() and re.match(r'.*\.BEA', f.name)]
+            # last_dir = sorted(msg_dirs)[-1]
+            # nowt = time.time()
+            # if os.path.isdir(last_dir):
+            #     for filename in os.scandir(last_dir):
+            #         if os.stat(filename).st_ctime > nowt - 6:
+            #             mesg = open(filename, 'r',
+            #                         encoding='cp1251')
+            #             text = mesg.read()
+            #             parse_file(filename.path.replace(pager_dir, ''), text)
             nowt = time.time()
-            if os.path.isdir(last_dir):
-                for filename in os.scandir(last_dir):
+            for root, dirs, files in os.walk(pager_dir):
+                for f in files:
+                    filename = os.path.join(root, f)
                     if os.stat(filename).st_ctime > nowt - 6:
                         mesg = open(filename, 'r',
                                     encoding='cp1251')
                         text = mesg.read()
-                        parse_file(filename.path.replace(pager_dir, ''), text)
-            msg_dirs = [f.path for f in os.scandir(pager_dir)
-                        if f.is_dir() and re.match(r'.*\.BEA', f.name)]
-            last_dir = sorted(msg_dirs)[-1]
-            nowt = time.time()
-            if os.path.isdir(last_dir):
-                for filename in os.scandir(last_dir):
-                    if os.stat(filename).st_ctime > nowt - 6:
-                        mesg = open(filename, 'r',
-                                    encoding='cp1251')
-                        text = mesg.read()
-                        parse_file(filename.path.replace(pager_dir, ''), text)
+                        parse_file(filename.replace(pager_dir, ''), text)
         except Exception as ex:
             logging.error(f'HFpager send/receive message error: {ex}')
             logging.debug(f'Error: {ex}', exc_info=True)
