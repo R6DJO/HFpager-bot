1934b51f46afa5b8f91312c75d7fbad3dcbdd738
Alexander Falin <alexander.falin@megafon.ru>
2022-04-23 22:18:02 +0300

    Add beacone

--- bot4.py
+++ bot4.py
@@ -57,7 +57,18 @@ def hfpager_bot():
                          'Documents/HFpager/')
             # pager_dir = '/storage/emulated/0/Documents/HFpager/'
             msg_dirs = [f.path for f in os.scandir(pager_dir)
-                        if f.is_dir() and re.match(r'.*\.MSG|.*\.BEA', f.name)]
+                        if f.is_dir() and re.match(r'.*\.MSG', f.name)]
+            last_dir = sorted(msg_dirs)[-1]
+            nowt = time.time()
+            if os.path.isdir(last_dir):
+                for filename in os.scandir(last_dir):
+                    if os.stat(filename).st_ctime > nowt - 6:
+                        mesg = open(filename, 'r',
+                                    encoding='cp1251')
+                        text = mesg.read()
+                        parse_file(filename.path.replace(pager_dir, ''), text)
+            msg_dirs = [f.path for f in os.scandir(pager_dir)
+                        if f.is_dir() and re.match(r'.*\.BEA', f.name)]
             last_dir = sorted(msg_dirs)[-1]
             nowt = time.time()
             if os.path.isdir(last_dir):
