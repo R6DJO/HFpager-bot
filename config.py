a77e5ed57d1d8b4c80ce1a1b9190f11700c77d08
unknown <xfalins@gamil.com>
2022-09-14 17:19:28 +0300

    Init

--- config.py
+++
@@ -1,8 +0,0 @@
-token = 'ТОКЕН'
-chat_id = ИД_ЧАТА
-beacon_chat_id = ИД_ЧАТА_МАЯКОВ
-my_id = ИД_МОЕГО_ПЕЙДЖЕРА
-abonent_id = ИД_ПЕЙДЖЕРА_КОРРЕСПОНДЕНТА_ПО_УМОЛЧАНИЮ
-owm_api_key = 'АПИ_КЕЙ_ОВМ'
-callsign = 'ПОЗЫВНОЙ_ШЛЮЗА'
-log_level = 'DEBUG'
